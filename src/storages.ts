1f37a8bd40528f1d69af5c595a12d4d82f5e78cb
oleksandravalko <oleksandra.valko@apify.com>
2024-11-01 10:52:36 +0100

    try RequestQueueV1

--- src/storages.ts
+++ src/storages.ts
@@ -1,5 +1,6 @@
 import { Actor } from 'apify';
+import { RequestQueueV1 } from 'crawlee';
 
-export const puppeteerRequestQueue = await Actor.openRequestQueue(
+export const puppeteerRequestQueue = await RequestQueueV1.open(
     `jobs-cz-detail-${Actor.isAtHome() ? Actor.getEnv().actorRunId : Date.now()}`,
 );
