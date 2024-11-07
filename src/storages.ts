6393a71fca6a6bea1028a3a03d36f4b0576eec38
oleksandravalko <oleksandra.valko@apify.com>
2024-11-07 16:28:17 +0100

    add logging

--- src/storages.ts
+++ src/storages.ts
@@ -1,16 +1,16 @@
-import { Actor, RequestQueue } from 'apify';
+import { Actor, log, RequestQueue } from 'apify';
 
 const savedId = await Actor.getValue<string>('RQ_ID');
-
+log.info(`RQ ID pulled from storage: ${savedId}`);
 const getNewId = async () => {
     const queueInfo = await Actor.apifyClient.requestQueues().getOrCreate();
     return queueInfo.id;
 };
 
 const currId = savedId || await getNewId();
-
 if (currId !== savedId) {
     await Actor.setValue('RQ_ID', currId);
 }
 
 export const puppeteerRequestQueue = await RequestQueue.open(currId);
+log.info(`ID of puppeteer RQ now: ${currId}`);
